b879c7e7c1a08a48f6154ba47ea4983147956a1a
Hongen Peng <1591999801@qq.com>
2026-08-06 18:28:45 +0800

    refactor(project): move active Matcher runtime to its independent repository

--- matcher/__init__.py
+++
@@ -1,5 +0,0 @@
-"""AdaLigand Stage2 Matcher。"""
-
-from .graph import GraphState, MolecularGraph, NodeEdgeLayer, build_molecular_graph
-
-__all__ = ["GraphState", "MolecularGraph", "NodeEdgeLayer", "build_molecular_graph"]
